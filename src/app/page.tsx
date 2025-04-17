40e1e99fef35315c6eb9d02c7f35314a02124edc
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-04-17 14:00:26 +0530

    modified components/ui: button input progress text-area checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">helloworld!</p>
+    <div className="p-4">
+      {" "}
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">BUTTON</Button>
+        </div>
+        <div>
+          <Input placeholder="INPUT" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="TEXT-AREA" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
