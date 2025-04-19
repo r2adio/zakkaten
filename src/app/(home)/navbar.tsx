f913c12c74dcd81f4b577fc66dde616048a6f4ec
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-04-19 15:51:47 +0530

    add mobile navbar, footer

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
 import { Poppins } from "next/font/google";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+
+import { NavbarSidebar } from "./navbar-sidebar";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
 
@@ -40,6 +44,8 @@ const navbarItems = [
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
@@ -47,6 +53,12 @@ export const Navbar = () => {
           zakkaten
         </span>
       </Link>
+
+      <NavbarSidebar
+        items={navbarItems}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
 
       <div className="items-center gap-4 hidden lg:flex">
         {navbarItems.map((item) => (
@@ -73,6 +85,16 @@ export const Navbar = () => {
           className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
         >
           <Link href="/sign-up">Start selling</Link>
+        </Button>
+      </div>
+
+      <div className="flex lg:hidden items-center justify-center">
+        <Button
+          variant="ghost"
+          className="size-12 border-transparent bg-white"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <MenuIcon />
         </Button>
       </div>
     </nav>
