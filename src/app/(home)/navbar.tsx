79cb7dc03cb248f6584e334e28cbc31ae2ab9bbd
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-04-19 11:55:12 +0530

    add home about features pricing contact pages

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,11 +1,45 @@
-import { Poppins } from "next/font/google";
+"use client";
+
 import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
 
+interface NavbarItemProps {
+  href: string;
+  children: React.ReactNode;
+  isActive?: boolean;
+}
+
+const NavbarItem = ({ href, children, isActive }: NavbarItemProps) => {
+  return (
+    <Button
+      asChild
+      variant="outline"
+      className={cn(
+        "bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg",
+        isActive && "bg-black text-white hover:bg-black hover:text-white",
+      )}
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
+};
+
+const navbarItems = [
+  { href: "/", children: "Home" },
+  { href: "/about", children: "About" },
+  { href: "/features", children: "Features" },
+  { href: "/pricing", children: "Pricing" },
+  { href: "/contact", children: "Contact" },
+];
+
 export const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
@@ -13,6 +47,18 @@ export const Navbar = () => {
           funroad
         </span>
       </Link>
+
+      <div className="items-center gap-4 hidden lg:flex">
+        {navbarItems.map((item) => (
+          <NavbarItem
+            key={item.href}
+            href={item.href}
+            isActive={pathname === item.href}
+          >
+            {item.children}
+          </NavbarItem>
+        ))}
+      </div>
     </nav>
   );
 };
