202cf8d0155bdba2df4218396c9299fbb0751fdb
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-04-19 12:12:55 +0530

    add login and start selling btn

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
         <span className={cn("text-5xl font-semibold", poppins.className)}>
-          funroad
+          zakkaten
         </span>
       </Link>
 
@@ -58,6 +58,22 @@ export const Navbar = () => {
             {item.children}
           </NavbarItem>
         ))}
+      </div>
+
+      <div className="hidden lg:flex">
+        <Button
+          asChild
+          variant="secondary"
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+        >
+          <Link href="/sign-in">Log-in</Link>
+        </Button>
+        <Button
+          asChild
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
+        >
+          <Link href="/sign-up">Start selling</Link>
+        </Button>
       </div>
     </nav>
   );
