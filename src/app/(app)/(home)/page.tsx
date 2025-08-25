b5db4885e93c2b300e24e8b618e49790761134e4
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-25 23:01:33 +0530

    feat: try adding a new collection, Categories

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,30 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-export default function Home() {
-  return (
-    <div className="p-4">
-      {" "}
-      <div className="flex flex-col gap-y-4">
-        <div>
-          <Button variant="elevated">BUTTON</Button>
-        </div>
-        <div>
-          <Input placeholder="INPUT" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea placeholder="TEXT-AREA" />
-        </div>
-        <div>
-          <Checkbox />
-        </div>
-      </div>
-    </div>
-  );
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+  });
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
