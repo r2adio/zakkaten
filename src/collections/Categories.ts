8c33d30fe25bbce139b8bdf514aea320f2bbacf1
gauravashisth <miya.musashi.moto1645@gmail.com>
2025-12-04 19:26:07 +0530

    chore: update dependencies

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -3,10 +3,21 @@ import type { CollectionConfig } from "payload";
 export const Categories: CollectionConfig = {
   slug: "categories",
   fields: [
+    { name: "name", type: "text", required: true },
+    { name: "slug", type: "text", required: true, unique: true, index: true },
+    { name: "color", type: "text" },
     {
-      name: "name",
-      type: "text",
-      required: true,
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories",
+      on: "parent", // the `subcategories` refers to the `parent`
+      hasMany: true,
     },
   ],
 };
